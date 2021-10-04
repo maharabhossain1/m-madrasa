72448537b71b791185e1ec66c09afd5c26bfd140
maharabhossain1 <maharabhossain0022@gmail.com>
2021-10-04 21:26:43 +0600

    fee added

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Card, Col } from "react-bootstrap";
 
 export default function Cart({ service }) {
-  const { title, img, text } = service;
+  const { title, img, text, fee } = service;
   return (
     <Col className="p-4">
       <Card style={{ width: "18rem" }}>
@@ -10,6 +10,7 @@ export default function Cart({ service }) {
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{text}</Card.Text>
+          <h5>Couse fee : {fee}</h5>
           <Button variant="outline-dark">Interest</Button>
         </Card.Body>
       </Card>
--- src/components/miniCart/MiniCart.js
+++ src/components/miniCart/MiniCart.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Card, Col } from "react-bootstrap";
 
 export default function MiniCart({ miniService }) {
-  const { title, img } = miniService;
+  const { title, img, fee } = miniService;
   return (
     <Col>
       <Card className="w-75">
         <Card.Img variant="top" src={img} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
+          <p>Course Fee : {fee} Taka</p>
         </Card.Body>
       </Card>
     </Col>
